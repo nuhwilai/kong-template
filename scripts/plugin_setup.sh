ccf88b7478984f0cf5875183de9b996795aa3db3
nuhwilai <new@bonmek.com>
2021-10-17 16:32:07 +0700

    clean unused plugin for template

--- scripts/plugin_setup.sh
+++ scripts/plugin_setup.sh
@@ -2,10 +2,8 @@
 
 echo "Starting to Install custom plugins"
 ## For each custom plugin
-## cd /usr/local/custom/kong/plugins/<plugin_name>
-## luarocks make
+## cd /usr/local/custom/kong/plugins/<folder_plugin_name> && luarocks make
 
-cd /usr/local/custom/kong/plugins/hide-token
-luarocks make
+#cd /usr/local/custom/kong/plugins/hide-token && luarocks make # example
 
 echo "Done Installing custom plugins"
